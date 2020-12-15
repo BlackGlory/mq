e9b4cf04ad85ac82dda5c69e55ed15a390e95dac
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-15 16:01:15 +0800

    feat: maintain first

--- src/core/mq.ts
+++ src/core/mq.ts
@@ -3,80 +3,64 @@ import { DRAFTING_TIMEOUT, ORDERED_TIMEOUT, ACTIVE_TIMEOUT, THROTTLE } from '@en
 import { nanoid } from 'nanoid'
 
 export async function draft(queueId: string, priority?: number): Promise<string> {
-  try {
-    const messageId = nanoid()
-    await MQDAO.draftMessage(queueId, messageId, priority)
-    return messageId
-  } finally {
-    await maintain(queueId)
-  }
+  await maintain(queueId)
+
+  const messageId = nanoid()
+  await MQDAO.draftMessage(queueId, messageId, priority)
+  return messageId
 }
 
 export async function set(queueId: string, messageId: string, type: string, payload: string): Promise<void> {
-  try {
-    await MQDAO.setMessage(queueId, messageId, type, payload)
-    queueMicrotask(() => SignalDAO.emit(queueId))
-  } finally {
-    await maintain(queueId)
-  }
+  await maintain(queueId)
+
+  await MQDAO.setMessage(queueId, messageId, type, payload)
+  queueMicrotask(() => SignalDAO.emit(queueId))
 }
 
 export async function order(queueId: string): Promise<string> {
-  try {
-    const configurations = await ConfigurationDAO.getConfigurations(queueId)
-    const throttle = THROTTLE()
-    const duration = configurations.throttle?.duration ?? throttle.duration
-    const limit = configurations.throttle?.limit ?? throttle.limit
+  await maintain(queueId)
 
-    while (true) {
-      const messageId = await MQDAO.orderMessage(queueId, duration, limit)
-      if (messageId) return messageId
-      await SignalDAO.wait(queueId)
-    }
-  } finally {
-    await maintain(queueId)
+  const configurations = await ConfigurationDAO.getConfigurations(queueId)
+  const throttle = THROTTLE()
+  const duration = configurations.throttle?.duration ?? throttle.duration
+  const limit = configurations.throttle?.limit ?? throttle.limit
+
+  while (true) {
+    const messageId = await MQDAO.orderMessage(queueId, duration, limit)
+    if (messageId) return messageId
+    await SignalDAO.wait(queueId)
   }
 }
 
 export async function get(queueId: string, messageId: string): Promise<IMessage> {
-  try {
-    const message = await MQDAO.getMessage(queueId, messageId)
-    return message
-  } finally {
-    await maintain(queueId)
-  }
+  await maintain(queueId)
+
+  const message = await MQDAO.getMessage(queueId, messageId)
+  return message
 }
 
 export async function complete(queueId: string, messageId: string): Promise<void> {
-  try {
-    await MQDAO.completeMessage(queueId, messageId)
-  } finally {
-    await maintain(queueId)
-  }
+  await maintain(queueId)
+
+  await MQDAO.completeMessage(queueId, messageId)
 }
 
 export async function abandon(queueId: string, messageId: string): Promise<void> {
-  try {
-    await MQDAO.abandonMessage(queueId, messageId)
-  } finally {
-    await maintain(queueId)
-  }
+  await maintain(queueId)
+
+  await MQDAO.abandonMessage(queueId, messageId)
 }
 
 export async function clear(queueId: string): Promise<void> {
-  try {
-    await MQDAO.clear(queueId)
-  } finally {
-    await maintain(queueId)
-  }
+  await maintain(queueId)
+
+  await MQDAO.clear(queueId)
 }
 
 export async function stats(queueId: string): Promise<IStats> {
-  try {
-    return await MQDAO.stats(queueId)
-  } finally {
-    await maintain(queueId)
-  }
+  await maintain(queueId)
+
+  return await MQDAO.stats(queueId)
 }
 
 async function maintain(queueId: string): Promise<void> {
