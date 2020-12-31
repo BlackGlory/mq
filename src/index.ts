3c1f03e1d132c0629837fae9dd3c33ab31eab155
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-01 01:46:23 +0800

    fix: process.on

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import { AbortController } from 'abort-controller'
-import { prepareDatabase as prepareConfigInSqlite3Database } from '@dao/config-in-sqlite3/database'
-import { prepareDatabase as prepareDataInSqlite3Database } from '@dao/data-in-sqlite3/database'
+import {
+  prepareDatabase as prepareConfigInSqlite3Database
+, closeDatabase as closeConfigInSqlite3Database
+} from '@dao/config-in-sqlite3/database'
+import {
+  prepareDatabase as prepareDataInSqlite3Database
+, closeDatabase as closeDataInSqlite3Database
+} from '@dao/data-in-sqlite3/database'
 import { autoMaintain } from '@core/mq'
 import { buildServer } from './server'
 import { PORT, HOST, CI } from '@env'
 
 const autoMaintainController = new AbortController()
 
-process.on('SIGHUP', () => {
+process.on('exit', () => {
   autoMaintainController.abort()
-  process.exit(1)
+  closeDataInSqlite3Database()
+  closeConfigInSqlite3Database()
 })
+process.on('SIGHUP', () => process.exit(128 + 1))
+process.on('SIGINT', () => process.exit(128 + 2))
+process.on('SIGTERM', () => process.exit(128 + 15))
 
 ;(async () => {
   await prepareConfigInSqlite3Database()
