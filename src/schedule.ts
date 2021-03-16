285662e9cf11bb5d01634fead8c61c48937ab1eb
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-03-16 14:32:37 +0800

    refactor: use extra-timers

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,18 +1,18 @@
 import { maintainAllQueues } from '@core/mq'
-import { delay, AbortError, withAbortSignal } from 'extra-promise'
+import { AbortError, withAbortSignal } from 'extra-promise'
+import { setDynamicTimeoutLoop } from 'extra-timers'
 import ms = require('ms')
 
 /**
  * 该函数是一个长时函数, 每个异步操作都应该可以响应AbortSignal以提前返回.
  */
-export async function maintainQueuesEverySecond(abortSignal: AbortSignal): Promise<void> {
-  while (true) {
+export function maintainQueuesEverySecond(abortSignal: AbortSignal): void {
+  const cancel = setDynamicTimeoutLoop(ms('1s'), async () => {
     try {
       await withAbortSignal(abortSignal, maintainAllQueues)
-      await withAbortSignal(abortSignal, () => delay(ms('1s')))
     } catch (e) {
-      if (e instanceof AbortError) break
+      if (e instanceof AbortError) return cancel()
       throw e
     }
-  }
+  })
 }
