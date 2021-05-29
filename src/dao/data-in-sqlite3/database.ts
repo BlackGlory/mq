c7c39afe77d71b6561aed1d836a5b5bbc1f526a4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-29 07:41:11 +0000

    perf: optimize

--- src/dao/data-in-sqlite3/database.ts
+++ src/dao/data-in-sqlite3/database.ts
@@ -29,5 +29,11 @@ export function getDatabase(): IDatabase {
 }
 
 export function closeDatabase(): void {
-  if (db) db.close()
+  if (db) {
+    db.exec(`
+      PRAGMA analysis_limit=400;
+      PRAGMA optimize;
+    `)
+    db.close()
+  }
 }
