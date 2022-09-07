b37594e6b5e7215c5d843e35350963f7739515b5
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-09-07 05:19:58 +0000

    refactor: use you-died

--- src/index.ts
+++ src/index.ts
@@ -5,32 +5,24 @@ import * as DataInSqlite3 from '@dao/data-in-sqlite3/database'
 import { callNextTickEverySecond } from './schedule'
 import { buildServer } from './server'
 import { PORT, HOST, NODE_ENV, NodeEnv } from '@env'
-
-const maintainController = new AbortController()
-
-process.on('exit', () => {
-  maintainController.abort()
-
-  DataInSqlite3.closeDatabase()
-  ConfigInSqlite3.closeDatabase()
-})
-
-process.on('SIGHUP', () => process.exit(128 + 1))
-process.on('SIGINT', () => process.exit(128 + 2))
-process.on('SIGTERM', () => process.exit(128 + 15))
+import { youDied } from 'you-died'
 
 go(async () => {
   ConfigInSqlite3.openDatabase()
+  youDied(() => ConfigInSqlite3.closeDatabase())
   await ConfigInSqlite3.prepareDatabase()
 
   DataInSqlite3.openDatabase()
+  youDied(() => DataInSqlite3.closeDatabase())
   await DataInSqlite3.prepareDatabase()
 
   const server = buildServer()
   await server.listen(PORT(), HOST())
   if (NODE_ENV() === NodeEnv.Test) process.exit()
 
+  const maintainController = new AbortController()
   callNextTickEverySecond(maintainController.signal)
+  youDied(() => maintainController.abort())
 
   process.send?.('ready')
 })
